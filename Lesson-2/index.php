0e1cbbb30ab886f36452f66916c74bf60521920e
Ali Ghanbarzadeh <mail.alighanbarzadeh@gmail.com>
2026-08-04 13:00:37 +0330

    Update index.php

--- Lesson-2/index.php
+++ Lesson-2/index.php
@@ -1,1 +1,31 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP page</title>
+</head>
+<body>
+
+    <?php
+    
+    require "header.php";
+
+    $name = "John";
+    ?>
+    <main>
+          <h1>Welcome <?php echo"$name"; ?></h1>
+              <?php 
+                 echo "this text comes from PHP.";
+                ?>
+    </main>
+    
+    <?php
+    include "footer.php"; 
+    ?>
+
+
+</body>
+</html>
