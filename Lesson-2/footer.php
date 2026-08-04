f5ec8c172eb0e334a4a2d28d80bd4ecf0d7b3afd
Ali Ghanbarzadeh <mail.alighanbarzadeh@gmail.com>
2026-08-04 13:01:47 +0330

    Add files via upload

---
+++ Lesson-2/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>Copyright 2026 - My PHP Website </p>
+</footer>
---
+++ Lesson-2/header.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+</header>
