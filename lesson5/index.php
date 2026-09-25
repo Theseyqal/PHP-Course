ce6e922cb716600d1ecbf033a5a62f6c3427e3e3
Ali Ghanbarzadeh <mail.alighanbarzadeh@gmail.com>
2026-09-25 12:36:22 +0330

    upload lesson 5

---
+++ lesson5/index.php
@@ -0,0 +1,91 @@
+<?php 
+
+// $price = 20 ;
+// $quantity = 3 ;
+
+// $total = $price * $quantity ;
+
+// echo $total ;
+
+// $number = 20 ;
+
+// echo $number % 3 ;
+
+// $count = 10 ;
+
+// $count += 5 ;
+// echo $count;
+
+// -=
+// *=
+// /=
+
+// $age = 20 ;
+// var_dump($age >= 18);
+
+// >
+// <
+// >=
+// <=
+
+// var_dump(5 == "5");
+
+// var_dump(5 === "5");
+
+// $age = 20 ; 
+// $hasTicket = true ;
+
+// var_dump($age >= 18 && $hasTicket === true);
+
+// ||
+// !
+
+// $age = 16 ;
+
+// if ($age >= 18){
+//     echo "You are an adult." ;
+// }else{
+//     echo "You are under 18." ;
+// }
+
+
+
+$score = 75 ;
+
+if($score >= 90 ){
+    echo "Exellent " ;
+}elseif ($score >= 70) {
+    echo "Good";
+}else {
+    echo " Keep practicing";
+}
+
+
+
+// * Login check 
+
+// $username = "Ali" ;
+// $password = "124";
+
+// if ($username === "Ali" && $password === "1234"){
+//     echo "Login successful !";
+// }else{
+//     echo "Invalid username or password .";
+// }
+
+// * Ternary operator 
+
+// $age  = 20 ;
+// $status = $age >= 18 ? "Adult" : "Under 18";
+
+// echo $status;
+
+// ? Mini challenge ---- Even or Odd
+
+// $number = 7 ;
+
+// if ( $number % 2 === 0){
+//     echo "Even";
+// }else {
+//     echo "odd";
+// }
